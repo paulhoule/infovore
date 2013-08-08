478a158097798577b6b033527a4f4b3051ec715f
Paul Houle <Paul Houle>
2013-08-08 19:06:45 -0400

    add some code that,  hypothetically,  would parse the incoming triples

--- bakemono/src/main/java/com/ontology2/bakemono/primitiveTriple/PrimitiveTripleInputFormat.java
+++ bakemono/src/main/java/com/ontology2/bakemono/primitiveTriple/PrimitiveTripleInputFormat.java
@@ -10,19 +10,21 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 import com.ontology2.millipede.primitiveTriples.PrimitiveTriple;
+import com.ontology2.millipede.primitiveTriples.PrimitiveTripleCodec;
 
 public class PrimitiveTripleInputFormat extends FileInputFormat<LongWritable,PrimitiveTriple> {
 
+    final static PrimitiveTripleCodec ptc=new PrimitiveTripleCodec();
+    
     @Override
     public RecordReader<LongWritable, PrimitiveTriple> createRecordReader(
             final InputSplit split, final TaskAttemptContext context) throws IOException,
             InterruptedException {
-        return new LineProcessingRecordReader() {
+        return new LineProcessingRecordReader<PrimitiveTriple>() {
 
             @Override
-            Object convert(Text line) {
-                // TODO Auto-generated method stub
-                return null;
+            PrimitiveTriple convert(Text line) {
+                return ptc.decode(line.toString());
             }
             
         };
