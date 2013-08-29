55c5fb76365586162280d784421e8ae8232b3d9f
Paul Houle <Paul Houle>
2013-08-29 17:02:48 -0400

    get the wiring working for the AmazonEMRCluster by blocking out a conflicting dependency on httpconfig that we got from jena

--- haruhi/src/main/java/com/ontology2/haruhi/AmazonEMRCluster.java
+++ haruhi/src/main/java/com/ontology2/haruhi/AmazonEMRCluster.java
@@ -5,13 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
 import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
+import com.amazonaws.services.elasticmapreduce.model.StepConfig;
 
 public class AmazonEMRCluster implements Cluster {
     private final JobFlowInstancesConfig instances;
     
     @Autowired private String awsSoftwareBucket;
     @Autowired private AmazonElasticMapReduce emrClient;
+    @Autowired private StepConfig debugStep;
+    @Autowired private String awsLogUri;
     
     public AmazonEMRCluster(JobFlowInstancesConfig instances) {
         this.instances = instances;
@@ -21,6 +26,16 @@ public class AmazonEMRCluster implements Cluster {
     public void runJob(MavenManagedJar defaultJar,List<String> jarArgs)
             throws Exception {
         String jarLocation=defaultJar.s3JarLocation(awsSoftwareBucket);
+        StepConfig[] steps = {
+                debugStep,
+                new StepConfig("main",new HadoopJarStepConfig(jarLocation).withArgs(jarArgs))
+        };
+        
+        RunJobFlowRequest that=new RunJobFlowRequest()
+            .withName("Haruhi submitted job")
+            .withSteps(steps)
+            .withLogUri(awsLogUri);
+        
     }
 
 }
