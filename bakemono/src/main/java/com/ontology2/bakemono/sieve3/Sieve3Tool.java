dfaebeb293001d4c18b8643310cc070b66be7fa8
Paul Houle <Paul Houle>
2013-09-16 16:23:48 -0400

    #26 this looks like a good first draft for the horizontal subdivision

--- bakemono/src/main/java/com/ontology2/bakemono/sieve3/Sieve3Tool.java
+++ bakemono/src/main/java/com/ontology2/bakemono/sieve3/Sieve3Tool.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.PeekingIterator;
 import com.ontology2.bakemono.Main;
@@ -106,15 +107,87 @@ public class Sieve3Tool implements Tool {
     
     public static Sieve3Configuration generateConfiguration() {
         return new Sieve3Configuration(
-                new Rule("a", matchesA())
+                new Rule("a", matchesA()),
+                new Rule("label", matchesLabel()),
+                new Rule("name", matchesTypeObjectName()),
+                new Rule("keyNs", inKeyNamespace()),
+                new Rule("key", matchesTypeObjectKey()),
+                new Rule("notableForPredicate",matchesNotableForPredicate()),
+                new Rule("description",matchesDescription()),
+                new Rule("text",matchesText()),
+                new Rule("webpages",matchesWebPage()),
+                new Rule("notability",isAboutNotability()),
+                new Rule("links",isLinkRelationship())
         );
     }
 
-    private static Predicate<PrimitiveTriple> matchesA() {
+    private static Predicate<PrimitiveTriple> matchesPredicate(final String that) {
         return new Predicate<PrimitiveTriple>() {
             @Override public boolean apply(PrimitiveTriple input) {
-                return input.predicate.equals("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>");
+                return input.predicate.equals(that);
+            }
+        };
+    }
+    
+    private static Predicate<PrimitiveTriple> predicateStartsWith(final String that) {
+        return new Predicate<PrimitiveTriple>() {
+            @Override public boolean apply(PrimitiveTriple input) {
+                return input.predicate.startsWith("<"+that);
+            }
+        };
+    }
+    
+    private static Predicate<PrimitiveTriple> matchesA() {
+        return matchesPredicate("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>");
+    };
+    
+    private static Predicate<PrimitiveTriple> matchesTypeObjectName() {
+        return matchesPredicate("<http://rdf.basekb.com/ns/type.object.name>");
+    };
+    
+    private static Predicate<PrimitiveTriple> matchesLabel() {
+        return matchesPredicate("<http://www.w3.org/2000/01/rdf-schema#label>");
+    };
+    
+    private static Predicate<PrimitiveTriple> inKeyNamespace() {
+        return predicateStartsWith("http://rdf.basekb.com/key/");
+    };
+    
+    private static Predicate<PrimitiveTriple> matchesTypeObjectKey() {
+        return matchesPredicate("<http://rdf.basekb.com/ns/type.object.key>");
+    };
+    
+    static Predicate<PrimitiveTriple> isLinkRelationship() {
+        return new Predicate<PrimitiveTriple>() {
+            @Override public boolean apply(PrimitiveTriple input) {
+                return input.object.startsWith("<") && input.object.endsWith(">");
             }
         };
     };
+    
+    private static Predicate<PrimitiveTriple> matchesNotableForPredicate() {
+        return matchesPredicate("<http://rdf.basekb.com/ns/common.notable_for.predicate>");
+    };
+    
+    private static Predicate<PrimitiveTriple> matchesDescription() {
+        return matchesPredicate("<http://rdf.basekb.com/ns/common.topic.description>");
+    };
+    
+    private static Predicate<PrimitiveTriple> matchesText() {
+        return matchesPredicate("<http://rdf.basekb.com/ns/common.document.text>");
+    };
+    
+    private static Predicate<PrimitiveTriple> matchesWebPage() {
+        return matchesPredicate("<http://rdf.basekb.com/ns/common.topic.topic_equivalent_webpage>");
+    };
+    
+    private static Predicate<PrimitiveTriple> isAboutNotability() {
+        return Predicates.or(
+                matchesPredicate("<http://rdf.basekb.com/ns/common.topic.notable_types>"),
+                matchesPredicate("<http://rdf.basekb.com/ns/common.notable_for.object>"),
+                matchesPredicate("<http://rdf.basekb.com/ns/common.topic.notable_for>"),
+                matchesPredicate("<http://rdf.basekb.com/ns/common.topic.notable_for.notable_object>")
+        );
+    };
+    
 }
--- bakemono/src/test/java/com/ontology2/bakemono/sieve3/Sieve3MapperTest.java
+++ bakemono/src/test/java/com/ontology2/bakemono/sieve3/Sieve3MapperTest.java
@@ -8,15 +8,40 @@ import java.io.IOException;
 import org.junit.Test;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
+import com.google.common.base.Predicate;
 import com.ontology2.bakemono.mapred.RealMultipleOutputs;
+import com.ontology2.centipede.primitiveTriples.PrimitiveTriple;
 
 public class Sieve3MapperTest {
 
+    @Test
     public void itClosesMultipleOutputs() throws IOException, InterruptedException {
         Sieve3Mapper s3m=new Sieve3Mapper();
         s3m.mos=mock(RealMultipleOutputs.class);
         s3m.cleanup(mock(Context.class));
         verify(s3m.mos).close();
+    }
+    
+    @Test
+    public void linkTriplesAreAcceptedAsSuch() {
+        PrimitiveTriple pt=new PrimitiveTriple(
+                "<http://example.com/A>",
+                "<http://example.com/B>",
+                "<http://example.com/C>"
+                );
+        
+        assertTrue(Sieve3Tool.isLinkRelationship().apply(pt));
+    }
+    
+    @Test
+    public void nonLinkTriplesTriplesAreNotAcceptedAsSuch() {
+        PrimitiveTriple pt=new PrimitiveTriple(
+                "<http://example.com/A>",
+                "<http://example.com/B>",
+                "55"
+                );
+        
+        assertFalse(Sieve3Tool.isLinkRelationship().apply(pt));
     }
 
 }
