2b4603093f2baff8f75cba18fd6ef174565d9446
Paul Houle <paul@ontology2.com>
2013-11-10 18:05:48 -0500

    initial work on #70 -- in particular add mapper that will extract URI objects from triples so we can extract the set of all distinct objects

---
+++ bakemono/src/main/java/com/ontology2/bakemono/mapmap/PTUniqueMapMapper.java
@@ -0,0 +1,38 @@
+package com.ontology2.bakemono.mapmap;
+
+import com.google.common.base.Function;
+import com.ontology2.bakemono.primitiveTriples.PrimitiveTriple;
+import com.ontology2.bakemono.primitiveTriples.PrimitiveTripleCodec;
+import com.ontology2.centipede.Codec;
+import org.apache.hadoop.io.LongWritable;
+
+import javax.annotation.Nullable;
+
+public abstract class PTUniqueMapMapper<OutKey>
+        extends MapMapper<PrimitiveTriple,OutKey,LongWritable> {
+
+    private final Codec<PrimitiveTriple> codec=
+            new PrimitiveTripleCodec();
+
+    private final Function<PrimitiveTriple,LongWritable> valueFunction=
+            new Function<PrimitiveTriple,LongWritable>() {
+                @Nullable
+                @Override
+                public LongWritable apply(@Nullable PrimitiveTriple primitiveTriple) {
+                    return ONE;
+                }
+            };
+
+    private final LongWritable ONE=new LongWritable(1);
+
+    @Override
+    Codec<PrimitiveTriple> getCodec() {
+        return codec;
+    };
+
+    @Override
+    Function<PrimitiveTriple,LongWritable> getValueFunction() {
+        return valueFunction;
+    }
+
+}
