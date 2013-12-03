760badae30619dc7847ca94aa03c86d03f1dcae1
Paul Houle <paul@ontology2.com>
2013-12-03 11:53:02 -0500

    add more logging instrumentation to find out why #82 isn't working

--- bakemono/src/main/java/com/ontology2/bakemono/diffFacts/DiffFactReducer.java
+++ bakemono/src/main/java/com/ontology2/bakemono/diffFacts/DiffFactReducer.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.VIntWritable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Set;
@@ -15,6 +16,7 @@ import java.util.Set;
 public class DiffFactReducer<KeyType extends WritableComparable>
         extends Reducer<TaggedItem<KeyType>,VIntWritable,Text,KeyType> {
 
+    final static Logger log= Logger.getLogger(DiffFactReducer.class);
     final static VIntWritable ONE=new VIntWritable(1);
     final static VIntWritable TWO=new VIntWritable(2);
 
@@ -24,6 +26,10 @@ public class DiffFactReducer<KeyType extends WritableComparable>
     @Override
     protected void reduce(TaggedItem<KeyType> key, Iterable<VIntWritable> values, Context context) throws IOException, InterruptedException {
         Set<VIntWritable> that= Sets.newHashSet();
+        for(VIntWritable tag:values) {
+            log.info("Saw key ["+key+"] with tag ["+tag+"]");
+        }
+
         Iterables.addAll(that, values);
 
         if(that.contains(ONE) & !that.contains(TWO)) {
