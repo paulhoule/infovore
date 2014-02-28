5b7db261fcf3c27c869942ae1574a001d7b52911
Paul Houle <paul@ontology2.com>
2014-02-28 17:50:00 -0500

    implement our own identity reducer

--- bakemono/src/main/java/com/ontology2/bakemono/dbpediaToBaseKB/DBpediaToBaseKBReducer.java
+++ bakemono/src/main/java/com/ontology2/bakemono/dbpediaToBaseKB/DBpediaToBaseKBReducer.java
@@ -1,7 +1,15 @@
 package com.ontology2.bakemono.dbpediaToBaseKB;
 
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.lib.IdentityReducer;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class DBpediaToBaseKBReducer extends IdentityReducer<Text,Text> {
+import java.io.IOException;
+
+public class DBpediaToBaseKBReducer extends Reducer<Text,Text,Text,Text> {
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        for(Text value:values)
+            context.write(key,value);
+        }
+    }
 }
