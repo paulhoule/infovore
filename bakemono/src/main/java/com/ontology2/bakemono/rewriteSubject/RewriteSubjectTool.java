a09bdf1e306f017f954375ab1628b44cd6e4d44e
Paul Houle <paul@ontology2.com>
2014-03-02 12:33:49 -0500

    give a name to the smushing tool

--- bakemono/src/main/java/com/ontology2/bakemono/rewriteSubject/RewriteSubjectTool.java
+++ bakemono/src/main/java/com/ontology2/bakemono/rewriteSubject/RewriteSubjectTool.java
@@ -3,8 +3,10 @@ package com.ontology2.bakemono.rewriteSubject;
 import com.ontology2.bakemono.mapreduce.InputPath;
 import com.ontology2.bakemono.mapreduce.SelfAwareTool;
 import com.ontology2.centipede.parser.Option;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component("smushSubject")
 public class RewriteSubjectTool extends SelfAwareTool<RewriteSubjectOptions> {
 }
