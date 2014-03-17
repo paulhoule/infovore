5c4dcf03d69d761d47293b07fcad63cbac33e026
Paul Houle <paul@ontology2.com>
2014-03-17 17:17:00 -0400

    specify the InputFormatClass because SelfAwareTool can't figure it out

--- bakemono/src/main/java/com/ontology2/bakemono/smushObject/SmushObjectTool.java
+++ bakemono/src/main/java/com/ontology2/bakemono/smushObject/SmushObjectTool.java
@@ -2,8 +2,14 @@ package com.ontology2.bakemono.smushObject;
 
 import com.ontology2.bakemono.mapreduce.SelfAwareTool;
 import com.ontology2.bakemono.rewriteSubject.RewriteSubjectOptions;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.springframework.stereotype.Component;
 
 @Component("smushObject")
 public class SmushObjectTool extends SelfAwareTool<RewriteSubjectOptions> {
+    @Override
+    public Class<? extends InputFormat> getInputFormatClass() {
+        return TextInputFormat.class;
+    }
 }
