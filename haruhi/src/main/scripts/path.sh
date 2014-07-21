ae51d016d6856ce5577c3a0e9010a9655f5e9248
Paul Houle <paul@ontology2.com>
2014-07-21 18:08:34 -0400

    switch to onejar packaging so,  hopefully,  spring can always find the XSD files it wants.

--- haruhi/src/main/scripts/path.sh
+++ haruhi/src/main/scripts/path.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-alias haruhi="java -jar $HOME/.m2/repository/com/ontology2/haruhi/${project.version}/haruhi-${project.version}-jar-with-dependencies.jar"
+alias haruhi="java -jar $HOME/.m2/repository/com/ontology2/haruhi/${project.version}/haruhi-${project.version}-onejar.jar"
 
