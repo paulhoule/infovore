f307dca4c6696f31ede8bdc890b4d138bacaa85a
Paul Houle <paul@ontology2.com>
2014-10-03 18:32:35 -0400

    make sign of partition always positive

--- bakemono/src/main/java/com/ontology2/bakemono/jena/PartitionOnSubject.java
+++ bakemono/src/main/java/com/ontology2/bakemono/jena/PartitionOnSubject.java
@@ -2,10 +2,12 @@ package com.ontology2.bakemono.jena;
 
 import org.apache.hadoop.mapreduce.Partitioner;
 
+import static java.lang.Math.*;
+
 public class PartitionOnSubject extends Partitioner<WritableTriple,WritableTriple> {
 
     @Override
     public int getPartition(WritableTriple key, WritableTriple value, int i) {
-        return key.getTriple().getSubject().toString().hashCode() % i;
+        return abs(key.getTriple().getSubject().toString().hashCode()) % i;
     }
 }
