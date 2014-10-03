c64b122dac58b79f705faf37281e0e05aa19cedc
Paul Houle <paul@ontology2.com>
2014-10-03 08:12:29 -0400

    add debug code to make sure we aren't more confused than I think we are

--- bakemono/src/main/java/com/ontology2/bakemono/pse3/PSE3Mapper.java
+++ bakemono/src/main/java/com/ontology2/bakemono/pse3/PSE3Mapper.java
@@ -47,6 +47,8 @@ public class PSE3Mapper extends Mapper<LongWritable,Text,WritableTriple,Writable
     @Override
     public void setup(Context context) throws IOException,
     InterruptedException {
+        logger.error("Really paranoid logger debug method");
+        logger.warn("It's not just that we ignore warnings");
         mos=new RealMultipleOutputs(context);
         super.setup(context);
         accepted=new PrimaryKeyValueAcceptor(context);
