a2849f725d0d6d50356315ad647feeea91f6dd80
Paul Houle <paul@ontology2.com>
2014-10-16 17:03:52 -0400

    upload to server so we can get timing data for download

--- haruhi/src/main/java/com/ontology2/haruhi/fetchLogs/FetchLogs.java
+++ haruhi/src/main/java/com/ontology2/haruhi/fetchLogs/FetchLogs.java
@@ -1,11 +1,16 @@
 package com.ontology2.haruhi.fetchLogs;
 
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressListener;
 import com.amazonaws.services.ec2.model.*;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.transfer.Transfer;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.ontology2.centipede.shell.CommandLineApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +19,12 @@ import java.io.File;
 
 import static com.google.common.base.Splitter.*;
 import static com.google.common.collect.Iterables.*;
+import static java.lang.Thread.*;
 
 @Component
 public class FetchLogs extends CommandLineApplication {
+    static Logger logger = LoggerFactory.getLogger(FetchLogs.class);
+
     @Autowired
     TransferManager transferManager;
     @Resource
@@ -26,33 +34,27 @@ public class FetchLogs extends CommandLineApplication {
 
     @Override
     protected void _run(String[] strings) throws Exception {
-        for(String that:strings) {
+        for (String that : strings) {
             doItForJob(that);
         }
-
-//        String instanceId="i-3c9b476f";
-//        String name="Testing "+System.currentTimeMillis();
-//        DescribeInstancesRequest dir= new DescribeInstancesRequest().withInstanceIds(instanceId);
-//        DescribeInstancesResult dir2=ec2Client.describeInstances(dir);
-//        Reservation rs=dir2.getReservations().get(0);
-//        Instance i=rs.getInstances().get(0);
-//        String instanceState=i.getState().getName().intern();
-//
-//        if(instanceState!="stopped") {
-//            System.out.println("You must stop this instance safely before I can image it.");
-//            System.exit(-1);
-//        }
-//
-//        CreateImageRequest request=new CreateImageRequest(instanceId,name).withBlockDeviceMappings(
-//                new BlockDeviceMapping().withVirtualName("ephemeral0").withDeviceName("/dev/xvdb"),
-//                new BlockDeviceMapping().withDeviceName("/dev/sdf").withEbs(new EbsBlockDevice().withDeleteOnTermination(true))
-//        );
-//        CreateImageResult result=ec2Client.createImage(request);
-//        System.out.println(result.getImageId());
     }
 
-    private void doItForJob(String jobId) {
+    private void doItForJob(String jobId) throws InterruptedException {
+        logger.info("Beginning download of "+jobId);
         String bucketName= getLast(on("/").omitEmptyStrings().split(awsLogUri));
-        transferManager.downloadDirectory(bucketName, jobId, new File(localLogTarget));
+        Transfer that=transferManager.downloadDirectory(bucketName, jobId, new File(localLogTarget));
+        that.addProgressListener(new ProgressListener() {
+            @Override
+            public void progressChanged(ProgressEvent progressEvent) {
+                long bytes=progressEvent.getBytesTransferred();
+                logger.info("Transferred %s bytes in download",bytes);
+            }
+        });
+        while(!that.isDone()) {
+            logger.info("Transferred %s % of download",that.getProgress().getPercentTransferred());
+            sleep(5000);
+        }
+
+        that.waitForCompletion();
     }
 }
