0ae2d0c99cc28ae35758dfc0ea7c1fad4bafa022
Paul Houle <paul@ontology2.com>
2014-10-31 10:53:42 -0400

    move ps3options (I think the last)

---
+++ bakemono-options/src/main/java/com/ontology2/bakemono/pse3/PSE3Options.java
@@ -0,0 +1,12 @@
+package com.ontology2.bakemono.pse3;
+
+import com.ontology2.bakemono.util.CommonOptions;
+import com.ontology2.centipede.parser.ContextualConverter;
+import com.ontology2.centipede.parser.HasOptions;
+import com.ontology2.centipede.parser.Option;
+
+import java.util.List;
+
+public class PSE3Options extends CommonOptions {
+}
+
